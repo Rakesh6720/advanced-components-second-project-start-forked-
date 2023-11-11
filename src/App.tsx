23a0d7e992c74b7019b75a0d4848a06a85d4b810
rbaruah <31023635+Rakesh6720@users.noreply.github.com>
2023-11-11 16:04:16 +0000

    Update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Button from "./components/Button";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <p>
+        <Button el="button">A Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          A Link
+        </Button>
+      </p>
+    </main>
+  );
 }
 
 export default App;
