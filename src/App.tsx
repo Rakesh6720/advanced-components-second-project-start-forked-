56d9484de36bc0ec3ccfd887c29ae8a427b70f8c
rbaruah <31023635+Rakesh6720@users.noreply.github.com>
2023-11-13 16:34:27 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { useRef } from "react";
 import Button from "./components/Button";
+import Form, { type FormHandle } from "./components/Form";
+import Input from "./components/Input";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+
   return (
     <main>
-      <p>
-        <Button el="button">A Button</Button>
-      </p>
-      <p>
-        <Button el="anchor" href="https://google.com">
-          A Link
-        </Button>
-      </p>
+      <Form onSave={handleSave}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button el="button">Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
